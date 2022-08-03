145c3c7a2eb92207996796d0ad89dec0781706aa
Archit Kumar <archit@wpi.edu>
2022-08-03 17:23:45 -0500

    Dynamically get last event

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ from pathlib import Path
 source_data = Path('.', 'sample_events.xlsx')  # set path & name of source file here
 wb = opex.load_workbook(source_data)
 sheet = wb.active
-end = sheet.max_row
+data_end_row = sheet.max_row
 curr_year = dt.date.today().year
 data_start_row = 2
 heads_up_days = 1   # set here how many days ahead you want a reminder
 heads_up_delta = dt.timedelta(days=heads_up_days)
 
 # parse sheet for any events that need a reminder
-for row_num in range(data_start_row, sheet.max_row+1):
+for row_num in range(data_start_row, data_end_row+1):
     event_day = int(sheet['A'+str(row_num)].value)
     event_month = int(sheet['B'+str(row_num)].value)
     event_date = dt.date(year=curr_year, month=event_month, day=event_day)
