dd679972e60a4c6fd49d439345447734b1ead4d3
Archit Kumar <archit@wpi.edu>
2023-04-11 20:03:19 -0500

    Show user event reminders table

--- appmain.py
+++ appmain.py
@@ -1,4 +1,12 @@
 from flask import Flask, render_template
+import pandas as pd
+import psycopg2
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+
+load_dotenv()
 
 
 app = Flask(__name__)
@@ -6,4 +14,18 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template("index.html")
+    conn = psycopg2.connect(database=os.environ.get("database_name"),
+                            user=os.environ.get("database_user"),
+                            password=os.environ.get("database_user_password"),
+                            host=os.environ.get("database_host"),
+                            port=os.environ.get("database_port"))
+
+    # read sql query to query database
+    with open(Path(Path(__file__).parents[0], "assets/sql/get_event_reminders.sql"), 'r') as f:
+        sql_query = f.read()
+    df = pd.read_sql_query(sql_query, conn)
+
+    df["event_date"] = pd.to_datetime(df["event_date"], format="%m-%d").dt.date
+    df["event_date"] = df["event_date"].apply(lambda x: x.replace(year=2023))
+
+    return render_template("index.html", rows=df.to_dict(orient='records'), columns=df.columns.to_list())
